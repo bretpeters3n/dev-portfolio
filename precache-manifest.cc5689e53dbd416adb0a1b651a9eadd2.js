58115634e93f7f45715958d649ff0451550e8d1d
Bret Petersen <bretpetersen82@gmail.com>
2022-01-25 21:09:48 -0600

    Adding react build files here for gh-pages

---
+++ precache-manifest.cc5689e53dbd416adb0a1b651a9eadd2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b6940ed077934d263c8f585ad2e261",
+    "url": "/index.html"
+  },
+  {
+    "revision": "664c980e694dd8429fe1",
+    "url": "/static/css/2.c3097103.chunk.css"
+  },
+  {
+    "revision": "84047677ebe9aaff038c",
+    "url": "/static/css/main.06ed247d.chunk.css"
+  },
+  {
+    "revision": "664c980e694dd8429fe1",
+    "url": "/static/js/2.8cb2c650.chunk.js"
+  },
+  {
+    "revision": "b044a876f80c79bb830d598fc2795d21",
+    "url": "/static/js/2.8cb2c650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84047677ebe9aaff038c",
+    "url": "/static/js/main.e8c98c97.chunk.js"
+  },
+  {
+    "revision": "f873c59778d6b33efa2b",
+    "url": "/static/js/runtime-main.0076e85b.js"
+  },
+  {
+    "revision": "21e9ce2cffc76810430c65f724252cbd",
+    "url": "/static/media/2022_bretpetersen_resume.21e9ce2c.pdf"
+  },
+  {
+    "revision": "5c383529ac0ebabd17eb3c7ff4654e8a",
+    "url": "/static/media/close-thin.5c383529.svg"
+  },
+  {
+    "revision": "eb9400a9f1cbad4e0ceca583128b0f48",
+    "url": "/static/media/code.eb9400a9.svg"
+  },
+  {
+    "revision": "61f5ef7ce90c56df299ca46d65e9be6d",
+    "url": "/static/media/email.61f5ef7c.svg"
+  },
+  {
+    "revision": "498a57272e7dac6ebff4daa738158861",
+    "url": "/static/media/github.498a5727.svg"
+  },
+  {
+    "revision": "b6740917badb285c884568c51493156a",
+    "url": "/static/media/hamburger-thin.b6740917.svg"
+  },
+  {
+    "revision": "47125dca64270027e4aab2193aaed48d",
+    "url": "/static/media/heroku.47125dca.svg"
+  },
+  {
+    "revision": "5e3f2b4014426e25140499856aa11e57",
+    "url": "/static/media/linkedin.5e3f2b40.svg"
+  },
+  {
+    "revision": "4ed2e8e95cba0775997c4a7e53c4213f",
+    "url": "/static/media/phone.4ed2e8e9.svg"
+  },
+  {
+    "revision": "60d6aa480873c3f7bd8731c0658eb955",
+    "url": "/static/media/project-catMemory.60d6aa48.png"
+  },
+  {
+    "revision": "c05b02c4ffeadefb3cbdbac72378080f",
+    "url": "/static/media/project-graphicDesign.c05b02c4.png"
+  },
+  {
+    "revision": "1770cf316fc4921b11fd2497635bab89",
+    "url": "/static/media/project-northerHealthTracker.1770cf31.png"
+  },
+  {
+    "revision": "5d141ce3c9529b48658a3f9a2f283c36",
+    "url": "/static/media/project-trueCrimeQuiz.5d141ce3.png"
+  }
+]);
