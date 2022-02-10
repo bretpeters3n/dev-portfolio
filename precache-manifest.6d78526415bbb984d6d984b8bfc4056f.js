e3f01126b06a90b38f518837e1adc1a645efc0db
Bret Petersen <bretpetersen82@gmail.com>
2022-02-10 14:43:24 -0600

    Updates

---
+++ precache-manifest.6d78526415bbb984d6d984b8bfc4056f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8abc1a36cf1e1509b13bf6c26fd87e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3fd8c613e70cf5804d5",
+    "url": "/static/css/2.c3097103.chunk.css"
+  },
+  {
+    "revision": "9953950ab4725ab39886",
+    "url": "/static/css/main.57942736.chunk.css"
+  },
+  {
+    "revision": "c3fd8c613e70cf5804d5",
+    "url": "/static/js/2.838ebdca.chunk.js"
+  },
+  {
+    "revision": "b044a876f80c79bb830d598fc2795d21",
+    "url": "/static/js/2.838ebdca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9953950ab4725ab39886",
+    "url": "/static/js/main.7ce1350c.chunk.js"
+  },
+  {
+    "revision": "bf9a3f2e24d058fbdfae",
+    "url": "/static/js/runtime-main.41386cbe.js"
+  },
+  {
+    "revision": "21e9ce2cffc76810430c65f724252cbd",
+    "url": "/static/media/2022_bretpetersen_resume.21e9ce2c.pdf"
+  },
+  {
+    "revision": "5c383529ac0ebabd17eb3c7ff4654e8a",
+    "url": "/static/media/close-thin.5c383529.svg"
+  },
+  {
+    "revision": "61f5ef7ce90c56df299ca46d65e9be6d",
+    "url": "/static/media/email.61f5ef7c.svg"
+  },
+  {
+    "revision": "498a57272e7dac6ebff4daa738158861",
+    "url": "/static/media/github.498a5727.svg"
+  },
+  {
+    "revision": "b6740917badb285c884568c51493156a",
+    "url": "/static/media/hamburger-thin.b6740917.svg"
+  },
+  {
+    "revision": "47125dca64270027e4aab2193aaed48d",
+    "url": "/static/media/heroku.47125dca.svg"
+  },
+  {
+    "revision": "0a26425bf559efaefdbcf63f20285cb4",
+    "url": "/static/media/html.0a26425b.svg"
+  },
+  {
+    "revision": "5e3f2b4014426e25140499856aa11e57",
+    "url": "/static/media/linkedin.5e3f2b40.svg"
+  },
+  {
+    "revision": "4ed2e8e95cba0775997c4a7e53c4213f",
+    "url": "/static/media/phone.4ed2e8e9.svg"
+  },
+  {
+    "revision": "60d6aa480873c3f7bd8731c0658eb955",
+    "url": "/static/media/project-catMemory.60d6aa48.png"
+  },
+  {
+    "revision": "c05b02c4ffeadefb3cbdbac72378080f",
+    "url": "/static/media/project-graphicDesign.c05b02c4.png"
+  },
+  {
+    "revision": "1770cf316fc4921b11fd2497635bab89",
+    "url": "/static/media/project-northerHealthTracker.1770cf31.png"
+  },
+  {
+    "revision": "5a8d08eb7a7c12d368fb6abd6284903e",
+    "url": "/static/media/project-noteTaker.5a8d08eb.png"
+  },
+  {
+    "revision": "5d141ce3c9529b48658a3f9a2f283c36",
+    "url": "/static/media/project-trueCrimeQuiz.5d141ce3.png"
+  }
+]);
